1365a92e5e608cc13ab07801409f939ec4cdc1a2
zenghh <625111833@qq.com>
2020-11-25 10:15:24 +0800

    jaas实例

---
+++ demo/src/main/java/core2/security/jaas/JASSTest.java
@@ -0,0 +1,13 @@
+package core2.security.jaas;
+/**
+ *
+ * @author zenghh
+ * @date 2020/11/25 10:14 AM
+ * @version 1.0
+ */
+public class JASSTest {
+    //TODO: 需要分别在两个jar包开发登录和操作模块
+    public static void main(String[] args) {
+
+    }
+}
